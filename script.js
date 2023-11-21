77cecb20645ba5b11c49ddbab4a15ad360b5b50c
koniatsu <koni.atsuo@gmail.com>
2023-11-21 14:46:36 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const isOnline = navigator.onLine;
 
-window.addEventListener("online", () => {
+console.log('isOnline = ' + isOnline);
+
+if(isOnline === true){
   document.querySelector(".results").innerHTML = "インターネットに接続中です";
+}else{
+  document.querySelector(".results").innerHTML = "インターネットから切断されました";
+}
+
+window.addEventListener("online", () => {
+  document.querySelector(".results2").innerHTML = "インターネットに接続中です";
 });
 window.addEventListener("offline", () => {
-  document.querySelector(".results").innerHTML = "インターネットから切断されました";
+  document.querySelector(".results2").innerHTML = "インターネットから切断されました";
 });
